b10a101d71a90d0cfdec2fc1e5b402932ea7cf05
milka <mileto94@abv.bg>
2014-07-23 17:51:56 +0300

    Add functional tests - pass

---
+++ functional_tests.py
@@ -0,0 +1,18 @@
+from cli.test import FunctionalTest
+import os
+from kalu_parser import PATH, VERSION
+import unittest
+
+
+class TestKaluParser(FunctionalTest):
+    def test_list(self):
+        print(PATH)
+        command = "./kalu_parser.py -v"
+        result = self.run_script(os.path.join(PATH, command))
+        result.stdout = result.stdout.decode('utf-8')
+        result.stderr = result.stderr.decode('utf-8')
+        self.assertScriptDoes(result, stdout=VERSION, trim_output=True)
+
+
+if __name__ == '__main__':
+    unittest.main()
