5b33ae3450d41c42d9ac252c5895ec046f441090
milka <mileto94@abv.bg>
2014-07-23 17:53:03 +0300

    Add small piece of code - FT pass

---
+++ kalu_parser.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import cli.app
+import os
+
+
+VERSION = "0.0001"
+PATH = os.getcwd()
+
+
+@cli.app.CommandLineApp
+def print_version(app):
+    if print_version.params.version:
+        print(VERSION)
+
+
+print_version.add_param("-v", "--version", help="show version",
+                        default=False, action="store_true")
+
+
+if __name__ == "__main__":
+    print_version.run()
