c1a79ae5b9707b001e455841d0dd8ee21e4d2fec
milka <mileto94@abv.bg>
2014-07-24 14:27:33 +0300

    Add simple code to pass FT

--- kalu_parser.py
+++ kalu_parser.py
@@ -5,14 +5,19 @@ import os
 
 VERSION = __file__ + " 0.0001"
 PATH = os.getcwd()
+HELP = """usage: print_version [-h]\n\noptional arguments:\n-h,
+        --help  show this help message and exit"""
 
 
 @cli.app.CommandLineApp
 def print_version(app):
     if print_version.params.version:
         print(VERSION)
+    else:
+        print(HELP)
 
 
+print_version.add_param("-H", "--Help", default=True, action="store_true")
 print_version.add_param("-v", "--version", help="show version",
                         default=False, action="store_true")
 
