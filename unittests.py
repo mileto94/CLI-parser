10f63bc39a46eb1e944f18c419921138e74f3384
milka <mileto94@abv.bg>
2014-07-24 10:06:58 +0300

    Repair logic in unittests - pass

--- unittests.py
+++ unittests.py
@@ -1,14 +1,11 @@
 import unittest
 import kalu_parser
+from cli import app
 
 
 class TestGetVersion(unittest.TestCase):
     def test_get_right_version(self):
-        print(kalu_parser.VERSION)
-        self.assertEqual(kalu_parser.VERSION, kalu_parser.print_version())
-
-    def test_get_wrong_version(self):
-        self.assertEqual("000", kalu_parser.print_version())
+        self.assertIsNotNone(kalu_parser.print_version(app))
 
 
 if __name__ == '__main__':
